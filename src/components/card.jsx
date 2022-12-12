32a35f4170bc9a47570f3a097d1941a625c24141
João Marcos Chaves <102380570+Joaomarcos7@users.noreply.github.com>
2022-12-11 21:15:30 -0300

    img no card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -19,9 +19,9 @@ const [bairros,setbairro]=useState('')
     return(
 
         <div className='card m-5'>
-        <img src="..." class="card-img-top card-image" id="image" alt="image"/> <ul class="list-group list-group-flush"> <li class="list-group-item fw-bold">Tipo:<span class="px-2" id="type">{props.Tipo}</span></li>  <li class="list-group-item fw-bold">Bairro:<span class="px-2 card-descricao" id="bairro">{bairros}</span></li>  <li class="list-group-item fw-bold">Data: <span class="px-2 card-descricao" type="date" id='date'>{props.Data}</span></li>  <li class="list-group-item fw-bold">Descrição: <span class="px-2 card-descricao" id="description" >{props.Descrição} </span></li>   </ul>  <div class="card-body d-flex justify-content-around"> <button type="button" class="btn btn-danger card-button"data-action='excluir' id={props.id} onClick={()=>removecard(props.id)}>Excluir</button> <button type="button" class="btn btn-warning card-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-action='alterar' id='alterar-${index}' onClick={()=>{document.querySelector('#CEP').dataset.index=props.id;}}  >Alterar</button> </div>
+        <img src="../../public/imgs/dia-mundial-do-meio-ambiente-1591367068-1280-2d36ebf0.jpg" class="card-img-top card-image" id="image" alt="image"/> <ul class="list-group list-group-flush"> <li class="list-group-item fw-bold">Tipo:<span class="px-2" id="type">{props.Tipo}</span></li>  <li class="list-group-item fw-bold">Bairro:<span class="px-2 card-descricao" id="bairro">{bairros}</span></li>  <li class="list-group-item fw-bold">Data: <span class="px-2 card-descricao" type="date" id='date'>{props.Data}</span></li>  <li class="list-group-item fw-bold">Descrição: <span class="px-2 card-descricao" id="description" >{props.Descrição} </span></li>   </ul>  <div class="card-body d-flex justify-content-around"> <button type="button" class="btn btn-danger card-button"data-action='excluir' id={props.id} onClick={()=>removecard(props.id)}>Excluir</button> <button type="button" class="btn btn-warning card-button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-action='alterar' id='alterar-${index}' onClick={()=>{document.querySelector('#CEP').dataset.index=props.id;}}  >Alterar</button> </div>
         </div>
-        
+
     )
 }
 
